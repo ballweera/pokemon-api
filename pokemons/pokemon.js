6fbde934430db1d79bdc3ad18020d6c61a3dc30e
Hemmarat Wachirahatthapong <hemmarat@go.buu.ac.th>
2019-10-05 16:21:40 +0700

    Use MongoDB as a persistance data source and use async/await

--- pokemons/pokemon.js
+++ pokemons/pokemon.js
@@ -1,3 +1,11 @@
+const mongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID
+
+const DB_URL = 'mongodb+srv://superadmin:pwdsecure@pokemon-cluster-zp5v5.gcp.mongodb.net/admin?retryWrites=true&w=majority'
+const DB_NAME = 'example'
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+let client
+
 let pokemons = []
 mockPokemon()
 
@@ -8,10 +16,50 @@ function Pokemon(name, type) {
     this.type2 = null
 }
 
-function savePokemon(name, type) {
+async function connectDatabase() {
+    if (client !== undefined && client !== null && client.isConnected) {
+        return client
+    }
+
+    client = await mongoClient.connect(DB_URL, options)
+        .catch(err => console.error(err))
+        
+    return client
+}
+
+async function getCollection(name) {
+    client = await connectDatabase().catch(err => console.error(err))
+    database = client.db(DB_NAME)
+    collection = database.collection(name)
+    return collection
+}
+
+async function getPokemons() {
+    var collection = await getCollection('pokemons')
+
+    try {
+        var result = await collection.find({}).toArray()
+        return result
+    } catch (err) {
+        console.error(err)
+        return null
+    } finally {
+        client.close()
+    }
+}
+
+async function savePokemon(name, type) {
     let p = createPokemon(name, type)
-    pokemons.push(p)
-    return true
+    var collection = await getCollection('pokemons')
+    try {
+        var result = await collection.insert(p)
+        return true
+    } catch (err) {
+        console.error(err)
+        return false
+    } finally {
+        client.close()
+    }
 }
 
 function createPokemon(name, type) {
@@ -30,19 +78,41 @@ function generateNewId(num) {
 }
 
 function isPokemonExisted(id) {
-    return pokemons[id-1] !== undefined && pokemons[id-1] !== null
+    return pokemons[id - 1] !== undefined && pokemons[id - 1] !== null
 }
 
-function getPokemon(id) {
-    return pokemons[id - 1]
+async function getPokemon(id) {
+    var collection = await getCollection('pokemons')
+
+    try {
+        var result = collection.findOne({ _id: ObjectID(id)})
+        return result
+    } catch(err) {
+        console.error(err)
+        return err
+    } finally {
+        // client.close()
+    }
 }
 
-function update(pokemon) {
-    pokemons[pokemon.id - 1] = pokemon
-    return true
+async function update(pokemon) {
+    let collection = await getCollection('pokemons')
+    
+    try {
+        var result = await collection.updateOne({ _id: ObjectID(pokemon._id)}, { $set: { type2: pokemon.type2 } })
+        return true
+    } catch(err) {
+        console.log(err)
+        return false
+    } finally {
+        // client.close()
+    }
+    // pokemons[pokemon.id - 1] = pokemon
+    // return true
 }
 
 module.exports.isPokemonExisted = isPokemonExisted
 module.exports.savePokemon = savePokemon
 module.exports.getPokemon = getPokemon
+module.exports.getPokemons = getPokemons
 module.exports.update = update
